82e01c9eb52162999671682a0e32bae6dbb8ed2e
basharwalid <111853307+basharwalid@users.noreply.github.com>
2023-12-21 21:33:20 +0200

    Initial commit

---
+++ app/src/main/java/com/bashar/sessiontwoassignment/Splash_Activity.java
@@ -0,0 +1,14 @@
+package com.bashar.sessiontwoassignment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
